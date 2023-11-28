f9a9ba2d073bccf94316b0f23dbb2c427378d54e
mnsajid007 <152244799+mnsajid007@users.noreply.github.com>
2023-11-28 12:01:39 +0530

    Update blogRoute.js

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -6,7 +6,7 @@ const route = express.Router();
 
 const storage = multer.diskStorage({
     destination (req, file, cb) {
-        cb(null, './client/public/files');
+        cb(null, './files');
     },
     filename (req, file, cb) {
         cb(null, Date.now() + file.originalname );
